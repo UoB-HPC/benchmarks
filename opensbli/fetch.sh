965db16cfb4b392fab8f680013a682d8b7fcded1
James Price <j.price@bristol.ac.uk>
2018-06-08 22:09:59 +0000

    Fix OpenSBLI buffer overrun bug
    Also remove undeeded python dependency installation.

--- opensbli/fetch.sh
+++ opensbli/fetch.sh
@@ -10,18 +10,5 @@ wget http://www.archer.ac.uk/community/benchmarks/archer/OpenSBLI_ARCHER_bench.z
 unzip OpenSBLI_ARCHER_bench.zip
 echo "ss 256 500 1 False" >OpenSBLI/Benchmark/input
 
-# Get other dependencies.
-PIP=pip
-if ! hash $PIP
-then
-    PIP=$HOME/.local/bin/pip
-    if ! hash $PIP
-    then
-        if ! easy_install --user pip
-        then
-            echo "Failed to find or install pip"
-            exit 1
-        fi
-    fi
-fi
-$PIP install --user -r requirements.txt
+# Fix a buffer overrun bug in the benchmark code
+sed -i 's/char type_of_simulation\[2\], HDF5op\[4\];/char type_of_simulation[3], HDF5op[6];/' OpenSBLI/Benchmark/OpenSBLI_ops.cpp
