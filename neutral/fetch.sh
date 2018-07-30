083d0a9e5e907fa54af7b18fad578cf29ec785e4
James Price <j.price@bristol.ac.uk>
2018-07-30 13:15:04 -0500

    [Neutral] Refactor BDW/SKL into a single script

--- neutral/fetch.sh
+++ neutral/fetch.sh
@@ -4,6 +4,8 @@ if [ ! -e arch/neutral/omp3/neutral.c ]
 then
     git clone https://github.com/UoB-HPC/arch
     cd arch
+    git checkout e8636f0ddc
+
     git clone https://github.com/UoB-HPC/neutral
     cd neutral
     git checkout 49c946bf54
