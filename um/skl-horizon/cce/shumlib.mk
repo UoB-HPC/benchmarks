38e8e7d542c97c1e69e35e12bbddc9c67346d8ef
James Price <j.price@bristol.ac.uk>
2018-08-24 16:51:12 -0500

    [UM] Add skl-horizon scripts and results

---
+++ um/skl-horizon/cce/shumlib.mk
@@ -0,0 +1,68 @@
+# Platform specific settings
+#-------------------------------------------------------------------------------
+
+# Make
+#-----
+# Make command
+MAKE=make
+
+# Fortran
+#--------
+# Compiler command
+FC=ftn
+# Precision flags (passed to all compilation commands)
+FCFLAGS_PREC=
+# Flag used to set OpenMP (passed to all compilation commands)
+FCFLAGS_OPENMP=-h omp
+# Flag used to unset OpenMP (passed to all compilation commands)
+FCFLAGS_NOOPENMP=-h noomp
+# Any other flags (to be passed to all compliation commands)
+FCFLAGS_EXTRA=-O2 -Ovector1 -hfp0 -hflex_mp=strict
+# Flag used to set PIC (Position-independent-code; required by dynamic lib
+# and so will only be passed to compile objects destined for the dynamic lib)
+FCFLAGS_PIC=-h pic
+# Flags used to toggle the building of a dynamic (shared) library
+FCFLAGS_SHARED=-shared -L${CRAYLIBS_X86_64} -lomp -lpgas-dmapp
+ifdef SHUM_OPENMP
+ifeq (${SHUM_OPENMP}, false)
+FCFLAGS_SHARED=-shared
+endif
+endif
+# Flags used for compiling a dynamically linked test executable; in some cases
+# control of this is argument order dependent - for these cases the first
+# variable will be inserted before the link commands and the second will be
+# inserted afterwards
+FCFLAGS_DYNAMIC=-dynamic
+FCFLAGS_DYNAMIC_TRAIL=-Wl,-rpath=${LIBDIR_OUT}/lib
+# Flags used for compiling a statically linked test executable (following the
+# same rules as the dynamic equivalents - see above comment)
+FCFLAGS_STATIC=
+FCFLAGS_STATIC_TRAIL=
+
+# C
+#--
+# Compiler command
+CC=cc
+# Precision flags (passed to all compilation commands)
+CCFLAGS_PREC=
+# Flag used to set OpenMP (passed to all compilation commands)
+CCFLAGS_OPENMP=-h omp
+# Flag used to unset OpenMP (passed to all compilation commands)
+CCFLAGS_NOOPENMP=-h noomp
+# Any other flags (to be passed to all compilation commands)
+CCFLAGS_EXTRA=-O3 -h c99 -hconform -hstdc -hnotolerant -hnognu -herror_on_warning
+# Flag used to set PIC (Position-independent-code; required by dynamic lib 
+# and so will only be passed to compile objects destined for the dynamic lib)
+CCFLAGS_PIC=-h pic
+
+# Archiver
+#---------
+# Archiver command
+AR=ar -rc
+
+# Set the name of this platform; this will be included as the name of the 
+# top-level directory in the build
+PLATFORM=skl-cce
+
+# Proceed to include the rest of the common makefile
+include ${DIR_ROOT}/Makefile
---
+++ um/skl-horizon/run.job
@@ -0,0 +1,61 @@
+#!/bin/bash
+#PBS -q batch
+#PBS -l nodes=1
+#PBS -l hostlist=\"[196-283]+[288-343]^\"
+#PBS -l walltime=00:30:00
+#PBS -joe
+
+rm -rf run
+mkdir run
+cd run
+cp -r $SRC_DIR/amip/data/* .
+cp -r $SRC_DIR/common/data/* .
+cp $SRC_DIR/common/scripts/wrapper .
+
+echo "JOB SCRIPT STARTING"
+
+export UMDIR="$UMDIR"
+export CUMFDIR="$CYLC_TASK_WORK_PATH"
+export DATAW="./"
+export DATAM="./"
+export INPUT_DATA="./"
+export FLUME_IOS_NPROC="0"
+export UM_ATM_NPROCX="8"
+export UM_ATM_NPROCY="5"
+export TOTAL_MPI_TASKS="$((UM_ATM_NPROCX*UM_ATM_NPROCY + FLUME_IOS_NPROC))"
+export OMP_NUM_THREADS="2"
+export OMP_STACKSIZE="2g"
+export STASHMASTER="./"
+export CORES_PER_NODE="40"
+export NUMA_REGIONS_PER_NODE="2"
+export HYPERTHREADS="2"
+export MPI_TASKS_PER_NODE="$((CORES_PER_NODE*HYPERTHREADS/OMP_NUM_THREADS))"
+export ROSE_LAUNCHER_PREOPTS="-ss -n $TOTAL_MPI_TASKS -N $MPI_TASKS_PER_NODE -S $((MPI_TASKS_PER_NODE/NUMA_REGIONS_PER_NODE)) -d $OMP_NUM_THREADS -j $HYPERTHREADS"
+export MPICH_COLL_SYNC="MPI_Gatherv"
+export FASTRUN=false
+
+module list 2>&1
+
+export ATMOS_KEEP_MPP_STDOUT=true
+export COUPLER=
+export DR_HOOK=0
+export DR_HOOK_CATCH_SIGNALS=0
+export DR_HOOK_IGNORE_SIGNALS=0
+export DR_HOOK_OPT=wallprof,self,time
+export DR_HOOK_PROFILE=./drhook.prof.cray.%d
+export DR_HOOK_PROFILE_LIMIT=-10.0
+export DR_HOOK_PROFILE_PROC=-1
+export ENS_MEMBER=0
+export HISTORY=./atmos.xhist
+export PRINT_STATUS=PrStatus_Min
+export RCF_PRINTSTATUS=PrStatus_Normal
+export RCF_TIMER=false
+export RECON_KEEP_MPP_STDOUT=true
+export RUNID=atmos
+export SPECTRAL_FILE_DIR=./spectral
+
+export PATH=$SRC_DIR/bin/:$PATH
+export LD_LIBRARY_PATH=$BUILD_DIR/shumlib/lib/:$LD_LIBRARY_PATH
+
+export ATMOS_LAUNCHER="aprun $ROSE_LAUNCHER_PREOPTS"
+./wrapper $BUILD_DIR/build-atmos/bin/um-atmos.exe
