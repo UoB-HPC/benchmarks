487555638d4cf5e4e136c0258c61ecec8ecece3f
James Price <j.price@bristol.ac.uk>
2018-12-04 10:25:48 +0000

    [CP2K] Update URL for source to GitHub release

--- cp2k/fetch.sh
+++ cp2k/fetch.sh
@@ -2,7 +2,7 @@
 
 if [ ! -e cp2k-5.1 ]
 then
-    wget https://netix.dl.sourceforge.net/project/cp2k/cp2k-5.1.tar.bz2
+    wget https://github.com/cp2k/cp2k/releases/download/v5.1.0/cp2k-5.1.tar.bz2
     tar xf cp2k-5.1.tar.bz2
 
     wget https://github.com/hfp/libxsmm/archive/1.9.tar.gz
