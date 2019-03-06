67c90b3c4714ba3bcd50b375ac854c73e7d37bf1
Tom Deakin <tom.deakin@bristol.ac.uk>
2019-03-06 11:33:30 +0000

    Download SNAP Kokkos version

--- snap/fetch.sh
+++ snap/fetch.sh
@@ -1,6 +1,46 @@
 #!/bin/bash
 
-if [ ! -e SNAP/src/snap_main.f90 ]
+DEFAULT_MODEL=omp
+function usage
+{
+    echo
+    echo "Usage: ./fetch [MODEL]"
+    echo
+    echo "Valid models:"
+    echo "  omp"
+    echo " kokkos"
+    echo
+    echo "The default programming model is '$DEFAULT_MODEL'."
+    echo
+}
+
+# Process arguments
+if [ $# -lt 1 ]
 then
-    git clone https://github.com/lanl/SNAP
+    usage
+    exit 1
 fi
+
+MODEL=${1:-$DEFAULT_MODEL}
+
+case "$MODEL" in
+  omp)
+    if [ ! -e SNAP/src/snap_main.f90 ]
+    then
+        git clone https://github.com/lanl/SNAP
+    fi
+    ;;
+  kokkos)
+    if [ ! -e SNAP-Kokkos/src/ext_sweep.cpp ]
+    then
+      git clone https://github.com/UoB-HPC/SNAP-Kokkos
+    fi
+    ;;
+  *)
+    echo
+    echo "Invalid model '$MODEL'."
+    usage
+    exit 1
+    ;;
+esac
+
