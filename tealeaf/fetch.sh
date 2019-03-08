10cc2b552476a3814ddd5a36e6b0a5707999d70c
Patrick Atkinson <pa13269@my.bristol.ac.uk>
2019-03-08 11:32:33 +0000

    adding more models to tealeaf fetch script

--- tealeaf/fetch.sh
+++ tealeaf/fetch.sh
@@ -1,6 +1,62 @@
 #!/bin/bash
 
-if [ ! -e TeaLeaf_ref/tea.f90 ]
+DEFAULT_MODEL=omp
+function usage
+{
+  echo
+  echo "Usage: ./fetch [MODEL]"
+  echo
+  echo "Valid models:"
+  echo " omp"
+  echo " omp-target"
+  echo " oacc"
+  echo " kokkos"
+  echo " raja"
+  echo " opencl"
+  echo
+  echo "The default programming model is '$DEFAULT_MODEL'."
+  echo
+}
+
+# Process arguments
+if [ $# -lt 1 ]
 then
-    git clone https://github.com/UK-MAC/TeaLeaf_ref
+  usage
+  exit 1
 fi
+
+MODEL=${1:-$DEFAULT_MODEL}
+
+case "$MODEL" in
+  omp)
+    if [ ! -e TeaLeaf_ref/tea.f90 ]
+    then
+      git clone https://github.com/lanl/SNAP
+    fi
+    ;;
+  kokkos|omp-target|oacc|raja)
+    if [ ! -e TeaLeaf/3d/main.c ]
+    then
+      git clone https://github.com/UoB-HPC/TeaLeaf
+    fi
+    ;;
+  cuda)
+    if [ ! -e TeaLeaf_CUDA/tea.f90 ]
+    then
+      https://github.com/UK-MAC/TeaLeaf_CUDA 
+    fi
+    ;;
+  opencl)
+    if [ ! -e TeaLeaf_OpenCL/tea.f90 ]
+    then
+      https://github.com/UK-MAC/TeaLeaf_OpenCL
+    fi
+    ;;
+  *)
+    echo
+    echo "Invalid model '$MODEL'."
+    usage
+    exit 1
+    ;;
+esac
+
