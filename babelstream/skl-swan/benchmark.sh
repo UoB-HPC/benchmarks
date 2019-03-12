395a00ba44b6898eefdfc57e0f8879ce125e71d6
Tom Deakin <tom.deakin@bristol.ac.uk>
2019-03-12 01:58:49 -0500

    Add BabelStream Skylake OpenMP script and results

---
+++ babelstream/skl-swan/benchmark.sh
@@ -0,0 +1,119 @@
+#!/bin/bash
+
+DEFAULT_COMPILER=intel-2018
+DEFAULT_MODEL=omp
+function usage
+{
+    echo
+    echo "Usage: ./benchmark.sh build|run [COMPILER] [MODEL]"
+    echo
+    echo "Valid compilers:"
+    echo "  cce-8.7"
+    echo "  gcc-7.3"
+    echo "  intel-2018"
+    echo
+    echo "Valid models:"
+    echo "  omp"
+    echo
+    echo "The default configuration is '$DEFAULT_COMPILER'."
+    echo "The default programming model is '$DEFAULT_MODEL'."
+    echo
+}
+
+# Process arguments
+if [ $# -lt 1 ]
+then
+    usage
+    exit 1
+fi
+
+ACTION=$1
+COMPILER=${2:-$DEFAULT_COMPILER}
+MODEL=${3:-$DEFAULT_MODEL}
+SCRIPT=`realpath $0`
+SCRIPT_DIR=`realpath $(dirname $SCRIPT)`
+
+export CONFIG="skl"_"$COMPILER"_"$MODEL"
+export BENCHMARK_EXE=stream-$CONFIG
+export SRC_DIR=$SCRIPT_DIR/BabelStream/
+export RUN_DIR=$SCRIPT_DIR
+
+
+# Set up the environment
+module swap craype-{broadwell,x86-skylake}
+case "$COMPILER" in
+    cce-8.7)
+        module swap cce cce/8.7.1
+        MAKE_OPTS='COMPILER=CRAY TARGET=CPU'
+        ;;
+    gcc-7.3)
+        module swap PrgEnv-{cray,gnu}
+        module swap gcc gcc/7.3.0
+        MAKE_OPTS='COMPILER=GNU TARET=CPU EXTRA_FLAGS="-march=skylake-avx512"'
+        ;;
+    intel-2018)
+        module swap PrgEnv-{cray,intel}
+        module swap intel intel/18.0.0.128
+        MAKE_OPTS='COMPILER=INTEL TARGET=CPU EXTRA_FLAGS=-xCORE-AVX512'
+        ;;
+    *)
+        echo
+        echo "Invalid compiler '$COMPILER'."
+        usage
+        exit 1
+        ;;
+esac
+
+
+# Handle actions
+if [ "$ACTION" == "build" ]
+then
+    # Fetch source code
+    if ! "$SCRIPT_DIR/../fetch.sh"
+    then
+        echo
+        echo "Failed to fetch source code."
+        echo
+        exit 1
+    fi
+
+    # Select Makefile to use
+    case "$MODEL" in
+      omp)
+        MAKE_FILE="OpenMP.make"
+        BINARY="omp-stream"
+    esac
+
+    # Perform build
+    if ! eval make -f $MAKE_FILE -C $SRC_DIR -B $MAKE_OPTS
+    then
+        echo
+        echo "Build failed."
+        echo
+        exit 1
+    fi
+
+    mkdir -p $RUN_DIR
+    # Rename binary
+    mv $SRC_DIR/$BINARY $RUN_DIR/$BENCHMARK_EXE
+
+elif [ "$ACTION" == "run" ]
+then
+    if [ ! -x "$RUN_DIR/$BENCHMARK_EXE" ]
+    then
+        echo "Executable '$RUN_DIR/$BENCHMARK_EXE' not found."
+        echo "Use the 'build' action first."
+        exit 1
+    fi
+
+    qsub $SCRIPT_DIR/run.job \
+        -d $RUN_DIR \
+        -o BabelStream-$CONFIG.out \
+        -N babelstream \
+        -V
+else
+    echo
+    echo "Invalid action (use 'build' or 'run')."
+    echo
+    exit 1
+fi
