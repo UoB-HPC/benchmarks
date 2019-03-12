999e979831b26ec442fbde22728d276873d08be3
Justin Lewis Salmon <justin.lewis.salmon@gmail.com>
2019-03-12 21:10:29 +0000

    [CloverLeaf/K20/BCP3] Add model support, fix for lack of realpath
    Signed-off-by: Justin Lewis Salmon <justin.lewis.salmon@gmail.com>

--- cloverleaf/k20-bcp3/benchmark.sh
+++ cloverleaf/k20-bcp3/benchmark.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 DEFAULT_COMPILER=gcc-7.1.0
+DEFAULT_MODEL=omp-target
 function usage
 {
     echo
@@ -9,7 +10,11 @@ function usage
     echo "Valid compilers:"
     echo "  gcc-7.1.0"
     echo
+    echo "Valid models:"
+    echo "  omp-target"
+    echo
     echo "The default configuration is '$DEFAULT_COMPILER'."
+    echo "The default programming model is '$DEFAULT_MODEL'."
     echo
 }
 
@@ -22,8 +27,9 @@ fi
 
 ACTION=$1
 COMPILER=${2:-$DEFAULT_COMPILER}
-SCRIPT=`realpath $0`
-SCRIPT_DIR=`realpath $(dirname $SCRIPT)`
+MODEL=${3:-$DEFAULT_MODEL}
+SCRIPT=`readlink -f $0`
+SCRIPT_DIR=`readlink -f $(dirname $SCRIPT)`
 
 export BENCHMARK_EXE=clover_leaf
 export CONFIG="k20"_"$COMPILER"
@@ -34,8 +40,7 @@ export RUN_DIR=$PWD/CloverLeaf-$CONFIG
 # Set up the environment
 case "$COMPILER" in
     gcc-7.1.0)
-        module swap PrgEnv-{cray,intel}
-        module swap intel intel/18.0.3.222
+        module load languages/gcc-7.1.0
         MAKE_OPTS='COMPILER=GNU MPI_C=mpicc MPI_F90=mpif90'
         ;;
     *)
@@ -51,7 +56,7 @@ esac
 if [ "$ACTION" == "build" ]
 then
     # Fetch source code
-    if ! "$SCRIPT_DIR/../fetch.sh"
+    if ! "$SCRIPT_DIR/../fetch.sh $MODEL"
     then
         echo
         echo "Failed to fetch source code."
