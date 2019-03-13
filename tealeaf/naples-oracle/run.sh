9fcbe336a55a33a0e820ec9fc3dabc3966fdafa8
Andrei Poenaru <ap13004@my.bristol.ac.uk>
2019-03-13 23:07:32 +0000

    [TeaLeaf] Add Naples scripts and results

---
+++ tealeaf/naples-oracle/run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -eu
+
+cp "$SRC_DIR/Benchmarks/tea_bm_5.in" tea.in
+
+export OMP_PROC_BIND=true OMP_PLACES=cores
+if [ "$MODEL" = omp ]; then
+    export OMP_NUM_THREADS=64
+    mpirun -np 1 --bind-to none "./$BENCHMARK_EXE"
+else
+    export OMP_NUM_THREADS=128
+    "./$BENCHMARK_EXE"
+fi
