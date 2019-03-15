9e998ab690da086fb5a525025abe19194d2815f2
Andrei Poenaru <ap13004@my.bristol.ac.uk>
2019-03-15 08:07:41 -0700

    [CloverLeaf] Add Ampere scripts and results

---
+++ cloverleaf/ampere-zoo/benchmark.sh
@@ -0,0 +1,103 @@
+#!/bin/bash
+
+set -e
+
+DEFAULT_COMPILER=gcc-8.1
+DEFAULT_MODEL=omp
+function usage
+{
+    echo
+    echo "Usage: ./benchmark.sh build|run [COMPILER] [MODEL]"
+    echo
+    echo "Valid compilers:"
+    echo "  gcc-8.1"
+    echo
+    echo "Valid models:"
+    echo "  mpi"
+    echo "  omp"
+    echo
+    echo "The default configuration is '$DEFAULT_COMPILER $DEFAULT_MODEL'."
+    echo
+}
+
+# Process arguments
+if [ $# -lt 1 ]
+then
+    usage
+    exit 1
+fi
+
+ACTION=$1
+COMPILER=${2:-$DEFAULT_COMPILER}
+export MODEL=${3:-$DEFAULT_MODEL}
+SCRIPT=`realpath $0`
+SCRIPT_DIR=`realpath $(dirname $SCRIPT)`
+
+export BENCHMARK_EXE=clover_leaf
+export CONFIG="ampere"_"$COMPILER"
+export SRC_DIR=$PWD/CloverLeaf_ref
+export RUN_DIR=$PWD/CloverLeaf-$CONFIG
+
+
+# Set up the environment
+module purge
+case "$COMPILER" in
+    gcc-8.1)
+        module load gcc/8.1 
+        module load openmpi/3.0.3/gcc-8.1
+        MAKE_OPTS='COMPILER=GNU'
+        MAKE_OPTS=$MAKE_OPTS' FLAGS_GNU="-Ofast -ffast-math -ffp-contract=fast -march=armv8-a -funroll-loops"'
+        MAKE_OPTS=$MAKE_OPTS' CFLAGS_GNU="-Ofast -ffast-math -ffp-contract=fast -march=armv8-a -funroll-loops"'
+        ;;
+    *)
+        echo
+        echo "Invalid compiler '$COMPILER'."
+        usage
+        exit 1
+        ;;
+esac
+
+
+# Handle actions
+if [ "$ACTION" == "build" ]
+then
+    # Fetch source code
+    if ! "$SCRIPT_DIR/../fetch.sh"
+    then
+        echo
+        echo "Failed to fetch source code."
+        echo
+        exit 1
+    fi
+
+    # Perform build
+    rm -f $SRC_DIR/$BENCHMARK_EXE $RUN_DIR/$BENCHMARK_EXE
+    make -C "$SRC_DIR" clean
+    if ! eval make -C $SRC_DIR -B $MAKE_OPTS
+    then
+        echo
+        echo "Build failed."
+        echo
+        exit 1
+    fi
+
+    mkdir -p $RUN_DIR
+    mv $SRC_DIR/$BENCHMARK_EXE $RUN_DIR
+
+elif [ "$ACTION" == "run" ]
+then
+    if [ ! -x "$RUN_DIR/$BENCHMARK_EXE" ]
+    then
+        echo "Executable '$RUN_DIR/$BENCHMARK_EXE' not found."
+        echo "Use the 'build' action first."
+        exit 1
+    fi
+
+    cd "$RUN_DIR"
+    bash "$SCRIPT_DIR"/run.sh
+else
+    echo
+    echo "Invalid action (use 'build' or 'run')."
+    echo
+    exit 1
+fi
---
+++ cloverleaf/ampere-zoo/run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -eu
+
+cp $SRC_DIR/InputDecks/clover_bm16.in clover.in
+
+case "$MODEL" in
+    mpi)
+        export OMP_NUM_THREADS=1
+        mpirun -np 32 --bind-to core ./$BENCHMARK_EXE
+        ;;
+    omp)
+        export OMP_NUM_THREADS=32 OMP_PROC_BIND=true OMP_PLACES=cores
+        mpirun -np 1 --bind-to none ./$BENCHMARK_EXE
+        ;;
+    *)
+        echo "Unknown run configuration for model '$MODEL'"
+        exit 1
+        ;;
+esac
+
