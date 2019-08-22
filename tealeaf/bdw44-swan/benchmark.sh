e224ca36de283d3596f92b81d9295b8918334ae3
James Price <j.price@bristol.ac.uk>
2019-08-22 13:39:31 -0500

    [TeaLeaf] Update x86 results
    Update compilers, use Swan for SKL40, use hybrid MPI/OpenMP.

---
+++ tealeaf/bdw44-swan/benchmark.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+function setup_env()
+{
+  PRGENV=`module -t list 2>&1 | grep PrgEnv`
+  case "$COMPILER" in
+      cce-9.0)
+          module swap $PRGENV PrgEnv-cray
+          module swap cce cce/9.0.1
+          MAKE_OPTS='COMPILER=CRAY MPI_COMPILER=ftn C_MPI_COMPILER=cc'
+          MAKE_OPTS=$MAKE_OPTS' FLAGS_CRAY="-em -ra -h acc_model=fast_addr:no_deep_copy:auto_async_all -homp"'
+          MAKE_OPTS=$MAKE_OPTS' CFLAGS_CRAY="-Ofast -fopenmp -march=broadwell -funroll-loops"'
+          ;;
+      gcc-8.3)
+          module swap $PRGENV PrgEnv-gnu
+          module swap gcc gcc/8.3.0
+          MAKE_OPTS='COMPILER=GNU MPI_COMPILER=ftn C_MPI_COMPILER=cc'
+          MAKE_OPTS=$MAKE_OPTS' FLAGS_GNU="-Ofast -march=broadwell -funroll-loops -cpp -ffree-line-length-none"'
+          MAKE_OPTS=$MAKE_OPTS' CFLAGS_GNU="-Ofast -march=broadwell -funroll-loops"'
+          ;;
+      intel-2019)
+          module swap $PRGENV PrgEnv-intel
+          module swap intel intel/19.0.3.199
+          MAKE_OPTS='COMPILER=INTEL MPI_COMPILER=ftn C_MPI_COMPILER=cc'
+          MAKE_OPTS=$MAKE_OPTS' FLAGS_INTEL="-O3 -no-prec-div -fpp -align array64byte -xCORE-AVX2"'
+          MAKE_OPTS=$MAKE_OPTS' CFLAGS_INTEL="-O3 -no-prec-div -restrict -fno-alias -xCORE-AVX2"'
+          ;;
+      *)
+          echo
+          echo "Invalid compiler '$COMPILER'."
+          usage
+          exit 1
+          ;;
+  esac
+}
+
+SCRIPT="`realpath $0`"
+export ARCH="bdw44"
+export PLATFORM_DIR="`realpath $(dirname $SCRIPT)`"
+export COMPILERS="cce-9.0 gcc-8.3 intel-2019"
+export DEFAULT_COMPILER=intel-2019
+export PBS_RESOURCES=":ncpus=44"
+export -f setup_env
+
+"$PLATFORM_DIR/../common.sh" "$@"
---
+++ tealeaf/skl56-swan/benchmark.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+function setup_env()
+{
+  module swap craype-{broadwell,x86-skylake}
+  PRGENV=`module -t list 2>&1 | grep PrgEnv`
+  case "$COMPILER" in
+      cce-9.0)
+          module swap $PRGENV PrgEnv-cray
+          module swap cce cce/9.0.1
+          MAKE_OPTS='COMPILER=CRAY MPI_COMPILER=ftn C_MPI_COMPILER=cc'
+          MAKE_OPTS=$MAKE_OPTS' FLAGS_CRAY="-em -ra -h acc_model=fast_addr:no_deep_copy:auto_async_all -homp"'
+          MAKE_OPTS=$MAKE_OPTS' CFLAGS_CRAY="-Ofast -fopenmp -march=skylake-avx512 -funroll-loops"'
+          ;;
+      gcc-8.3)
+          module swap $PRGENV PrgEnv-gnu
+          module swap gcc gcc/8.3.0
+          MAKE_OPTS='COMPILER=GNU MPI_COMPILER=ftn C_MPI_COMPILER=cc'
+          MAKE_OPTS=$MAKE_OPTS' FLAGS_GNU="-Ofast -march=skylake-avx512 -funroll-loops -cpp -ffree-line-length-none"'
+          MAKE_OPTS=$MAKE_OPTS' CFLAGS_GNU="-Ofast -march=skylake-avx512 -funroll-loops"'
+          ;;
+      intel-2019)
+          module swap $PRGENV PrgEnv-intel
+          module swap intel intel/19.0.3.199
+          MAKE_OPTS='COMPILER=INTEL MPI_COMPILER=ftn C_MPI_COMPILER=cc'
+          MAKE_OPTS=$MAKE_OPTS' FLAGS_INTEL="-O3 -no-prec-div -fpp -align array64byte -xCORE-AVX512"'
+          MAKE_OPTS=$MAKE_OPTS' CFLAGS_INTEL="-O3 -no-prec-div -restrict -fno-alias -xCORE-AVX512"'
+          ;;
+      *)
+          echo
+          echo "Invalid compiler '$COMPILER'."
+          usage
+          exit 1
+          ;;
+  esac
+}
+
+SCRIPT="`realpath $0`"
+export ARCH="skl56"
+export PLATFORM_DIR="`realpath $(dirname $SCRIPT)`"
+export COMPILERS="cce-9.0 gcc-8.3 intel-2019"
+export DEFAULT_COMPILER=intel-2019
+export PBS_RESOURCES=":ncpus=56"
+export -f setup_env
+
+"$PLATFORM_DIR/../common.sh" "$@"
