b280a8fc089d75b9020c2a1e9ddb3d4e95193db9
James Price <j.price@bristol.ac.uk>
2019-08-29 20:57:30 +0000

    [OpenSBLI] Update Isambard results
    Use latest compilers.

--- opensbli/tx2-isambard/benchmark.sh
+++ opensbli/tx2-isambard/benchmark.sh
@@ -4,32 +4,35 @@ function setup_env()
 {
   PRGENV=`module -t list 2>&1 | grep PrgEnv`
   case "$COMPILER" in
-      cce-8.7)
+      cce-9.0)
+          module load cdt/19.08
           module swap $PRGENV PrgEnv-cray
-          module swap cce cce/8.7.9
-          module load cray-hdf5-parallel/1.10.2.0
+          module swap cce cce/9.0.1
+          module load cray-hdf5-parallel/1.10.5.0
           export HDF5_INSTALL_PATH=$HDF5_DIR
           export OPS_COMPILER=cray
-          OPS_MAKE_OPTS=''
-          SBLI_MAKE_OPTS=''
+          OPS_MAKE_OPTS='MPICC=cc MPICXX=CC CXXFLAGS="-O3 -fPIC -DUNIX -Wall -ffp-contract=fast"'
+          SBLI_MAKE_OPTS='MPICPP=CC CCFLAGS="-O3 -fPIC -DUNIX -Wall -ffp-contract=fast"'
           ;;
-      gcc-8.2)
+      gcc-8.3)
+          module load cdt/19.08
           module swap $PRGENV PrgEnv-gnu
-          module swap gcc gcc/8.2.0
-          module load cray-hdf5-parallel/1.10.2.0
+          module swap gcc gcc/8.3.0
+          module load cray-hdf5-parallel/1.10.5.0
           export HDF5_INSTALL_PATH=$HDF5_DIR
           export OPS_COMPILER=gnu
           OPS_MAKE_OPTS='CC=cc CXX=CC MPICPP=CC MPICC=cc MPICXX=CC CXXFLAGS="-O3 -fPIC -DUNIX -Wall -ffloat-store -mcpu=thunderx2t99 -ffp-contract=fast"'
           SBLI_MAKE_OPTS='CC=cc CXX=CC MPICPP=CC MPICC=cc MPICXX=CC CCFLAGS="-O3 -fPIC -DUNIX -Wall -mcpu=thunderx2t99 -ffp-contract=fast"'
           ;;
-      arm-19.0)
-          module swpa $PRGENV PrgEnv-allinea
-          module swap allinea allinea/19.0.0.1
-          module load cray-hdf5-parallel/1.10.2.0
+      arm-19.2)
+          module load cdt/19.08
+          module swap $PRGENV PrgEnv-allinea
+          module swap allinea allinea/19.2.0.0
+          module load cray-hdf5-parallel/1.10.5.0
           export HDF5_INSTALL_PATH=$HDF5_DIR
           export OPS_COMPILER=gnu
-          OPS_MAKE_OPTS='CC=cc CXX=CC MPICPP=cc MPICC=cc MPICXX=CC CXXFLAGS="-O3 -fPIC -DUNIX -Wall -ffloat-store -mcpu=thunderx2t99 -ffp-contract=fast"'
-          SBLI_MAKE_OPTS='CC=cc CXX=CC MPICPP=cc MPICC=cc MPICXX=CC CCFLAGS="-O3 -fPIC -DUNIX -Wall -mcpu=thunderx2t99 -ffp-contract=fast"'
+          OPS_MAKE_OPTS='CC=cc CXX=CC MPICPP=CC MPICC=cc MPICXX=CC CXXFLAGS="-O3 -fPIC -DUNIX -Wall -mcpu=thunderx2t99 -ffp-contract=fast"'
+          SBLI_MAKE_OPTS='CC=cc CXX=CC MPICPP=CC MPICC=cc MPICXX=CC CCFLAGS="-O3 -fPIC -DUNIX -Wall -mcpu=thunderx2t99 -ffp-contract=fast"'
           ;;
       *)
           echo
@@ -43,8 +46,8 @@ function setup_env()
 SCRIPT="`realpath $0`"
 export ARCH="tx2"
 export PLATFORM_DIR="`realpath $(dirname $SCRIPT)`"
-export COMPILERS="cce-8.7 gcc-8.2 arm-19.0"
-export DEFAULT_COMPILER=cce-8.7
+export COMPILERS="cce-9.0 gcc-8.3 arm-19.2"
+export DEFAULT_COMPILER=cce-9.0
 export PBS_RESOURCES=":ncpus=64"
 export -f setup_env
 
