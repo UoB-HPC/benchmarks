5bbe2e40ba965cb16f6a6e784fa01c765b3ff6d2
James Price <j.price@bristol.ac.uk>
2019-08-29 20:38:24 +0000

    [SNAP] Update Isambard results
    Use latest compilers.

--- snap/tx2-isambard/benchmark.sh
+++ snap/tx2-isambard/benchmark.sh
@@ -6,21 +6,24 @@ function setup_env()
 {
   PRGENV=`module -t list 2>&1 | grep PrgEnv`
   case "$COMPILER" in
-      cce-8.7)
+      cce-9.0)
+          module load cdt/19.08
           module swap $PRGENV PrgEnv-cray
-          module swap cce cce/8.7.9
+          module swap cce cce/9.0.1
           export BENCHMARK_EXE=csnap
-          MAKE_OPTS='TARGET=csnap FORTRAN=ftn FFLAGS=-hfp3 PP=cpp'
+          MAKE_OPTS='TARGET=csnap FORTRAN=ftn FFLAGS="-hfp3 -homp" PP=cpp'
           ;;
-      gcc-8.2)
+      gcc-8.3)
+          module load cdt/19.08
           module swap $PRGENV PrgEnv-gnu
-          module swap gcc gcc/8.2.0
+          module swap gcc gcc/8.3.0
           export BENCHMARK_EXE=gsnap
           MAKE_OPTS='TARGET=gsnap FORTRAN=ftn FFLAGS="-Ofast -mcpu=native -fopenmp"'
           ;;
-      arm-19.0)
+      arm-19.2)
+          module load cdt/19.08
           module swap $PRGENV PrgEnv-allinea
-          module swap allinea allinea/19.0.0.1
+          module swap allinea allinea/19.2.0.0
           export BENCHMARK_EXE=gsnap
           MAKE_OPTS='TARGET=gsnap FORTRAN=ftn FFLAGS="-Ofast -mcpu=native -fopenmp"'
           ;;
@@ -36,8 +39,8 @@ function setup_env()
 SCRIPT="`realpath $0`"
 export ARCH="tx2"
 export PLATFORM_DIR="`realpath $(dirname $SCRIPT)`"
-export COMPILERS="cce-8.7 gcc-8.2 arm-19.0"
-export DEFAULT_COMPILER=cce-8.7
+export COMPILERS="cce-9.0 gcc-8.3 arm-19.2"
+export DEFAULT_COMPILER=cce-9.0
 export PBS_RESOURCES=":ncpus=64"
 export -f setup_env
 
