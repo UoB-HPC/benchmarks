257a2e87aabc569226bbe3313a7d86cd034193ae
James Price <j.price@bristol.ac.uk>
2019-08-29 21:08:40 +0000

    [TeaLeaf] Update Isambard results
    Use latest compilers, use hybrid OpenMP/MPI.

--- tealeaf/tx2-isambard/benchmark.sh
+++ tealeaf/tx2-isambard/benchmark.sh
@@ -1,25 +1,29 @@
 #!/bin/bash
 
-
 function setup_env()
 {
   PRGENV=`module -t list 2>&1 | grep PrgEnv`
   case "$COMPILER" in
-      cce-8.7)
+      cce-9.0)
+          module load cdt/19.08
           module swap $PRGENV PrgEnv-cray
-          module swap cce cce/8.7.9
+          module swap cce cce/9.0.1
           MAKE_OPTS='COMPILER=CRAY MPI_COMPILER=ftn C_MPI_COMPILER=cc'
+          MAKE_OPTS=$MAKE_OPTS' FLAGS_CRAY="-em -ra -h acc_model=fast_addr:no_deep_copy:auto_async_all -homp"'
+          MAKE_OPTS=$MAKE_OPTS' CFLAGS_CRAY="-Ofast -fopenmp -funroll-loops"'
           ;;
-      gcc-8.2)
+      gcc-8.3)
+          module load cdt/19.08
           module swap $PRGENV PrgEnv-gnu
-          module swap gcc gcc/8.2.0
+          module swap gcc gcc/8.3.0
           MAKE_OPTS='COMPILER=GNU MPI_COMPILER=ftn C_MPI_COMPILER=cc'
           MAKE_OPTS=$MAKE_OPTS' FLAGS_GNU="-Ofast -mcpu=native -funroll-loops -cpp -ffree-line-length-none"'
           MAKE_OPTS=$MAKE_OPTS' CFLAGS_GNU="-Ofast -mcpu=native -funroll-loops"'
           ;;
-      arm-19.0)
+      arm-19.2)
+          module load cdt/19.08
           module swap $PRGENV PrgEnv-allinea
-          module swap allinea allinea/19.0.0.1
+          module swap allinea allinea/19.2.0.0
           MAKE_OPTS='COMPILER=GNU MPI_COMPILER=ftn C_MPI_COMPILER=cc'
           MAKE_OPTS=$MAKE_OPTS' FLAGS_GNU="-Ofast -mcpu=native -funroll-loops -cpp -ffree-line-length-none"'
           MAKE_OPTS=$MAKE_OPTS' CFLAGS_GNU="-Ofast -mcpu=native -funroll-loops"'
@@ -36,8 +40,8 @@ function setup_env()
 SCRIPT="`realpath $0`"
 export ARCH="tx2"
 export PLATFORM_DIR="`realpath $(dirname $SCRIPT)`"
-export COMPILERS="cce-8.7 gcc-8.2 arm-19.0"
-export DEFAULT_COMPILER=cce-8.7
+export COMPILERS="cce-9.0 gcc-8.3 arm-19.2"
+export DEFAULT_COMPILER=cce-9.0
 export PBS_RESOURCES=":ncpus=64"
 export -f setup_env
 
